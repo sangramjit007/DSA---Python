946478140e01f090437428fa7195ecb2703aa418
My-Cosmos <sangram1alt2acc3io@gmail.com>
2026-01-04 13:35:49 +0530

    Dictionary & AdjacencyList Representation of Graph

--- Graph/Representation/ListRepresentationOfGraph.py
+++ Graph/Representation/ListRepresentationOfGraph.py
@@ -1,20 +1,19 @@
+# Adjacency List Representation of Graph
 def pr():
-    for i in range(len(matrix[0])):
-        for j in range(len(matrix)):
-            print(matrix[i][j], end="\t")
-        print("")
+    for i in range(len(lst)):
+        print(i, " -> ",lst[i])
 n = 5 # nodes/vertices
 m = 6 # edges
 edges = [[1,2], [2,4], [3,4], [1,3], [3,5],[5,4]]
 # 1 based index graph
-matrix = [[0 for _ in range(n+1)] for i in range(n+1)] # List Comprehension
+lst = [[] for _ in range(n+1)]
 # Before Operation printing matrix
 pr()
 print("--------------------------------------")
 for u, v in edges: # unpacking the list
     # This is a Undirected Graph
-    matrix[u][v] = 1
-    matrix[v][u] = 1
+    lst[u].append(v)
+    lst[v].append(u)
 # After Operation printing matrix
 pr()
 
